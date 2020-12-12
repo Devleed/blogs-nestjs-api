992fa717a0fb4098a754078fde878b206e2df211
devleed <mujahidwaleed11@gmail.com>
2020-12-12 12:32:10 +0500

    initial

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,21 +1,12 @@
-import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
-import { LocalAuthGuard } from './auth/local-auth.guard';
-import { AuthService } from './auth/auth.service';
-import { JwtAuthGuard } from './auth/jwt-auth.guart';
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private authService: AuthService) {}
+  constructor(private appService: AppService) {}
 
-  @UseGuards(LocalAuthGuard)
-  @Post('auth/login')
-  async login(@Request() req) {
-    return this.authService.login(req.user);
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Get('profile')
-  getProfile(@Request() req) {
-    return req.user;
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,18 +3,16 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
 import { BlogModule } from './blogs/blog.module';
 
 @Module({
   imports: [
-    AuthModule,
     BlogModule,
     MongooseModule.forRoot(
       'mongodb+srv://waleed:test@cluster0.4e26b.mongodb.net/blogs?retryWrites=true&w=majority',
     ),
   ],
   controllers: [AppController],
-  providers: [AppService, ],
+  providers: [AppService],
 })
 export class AppModule {}
