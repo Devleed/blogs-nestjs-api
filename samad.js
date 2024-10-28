0c669b719db44672201e6b60b587592459c23081
akira <akira@drox.com>
2024-10-28 11:41:49 +0000

    add samad.js as class component on samad branch

---
+++ samad.js
@@ -0,0 +1,13 @@
+// samad.js
+
+import React, { Component } from 'react';
+
+class Samad extends Component {
+    render() {
+        return (
+            <h1>Samad</h1>
+        );
+    }
+}
+
+export default Samad;
