dca51ac426c89b2611fe25320145bb35d7e07cf7
akira <akira@drox.com>
2024-10-28 12:32:59 +0000

    add bilal.js with class component

---
+++ Bilal.js
@@ -0,0 +1,13 @@
+// src/Bilal.js
+
+import React, { Component } from 'react';
+
+class Bilal extends Component {
+    render() {
+        return (
+            <h1>Bilal</h1>
+        );
+    }
+}
+
+export default Bilal;
